e5effb93e1cddf876e60b61e853f2b6a6da8b606
Rockford Wei <rocky@perfect.org>
2018-04-30 09:03:10 -0400

    Upgrading to Swift 4.1

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "PerfectArcade",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+        .Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 3),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
     ]
 )
